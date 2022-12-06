ec99de7e31c620790b98705a5e425427397eaad3
Aman Singh <reachout.amansingh@gmail.com>
2022-12-06 22:35:30 +0530

    feat : signup user input

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const formDetails = [
+const signupFormDetails = [
   {
     id: 1,
     label: "Email",
@@ -16,22 +16,41 @@ const formDetails = [
   },
 ];
 
+const initialFormDetails = {
+  email: "",
+  password: "",
+};
+
 export const Signup = () => {
+  const [formDetails, setFormDetails] = useState(initialFormDetails);
+
+  const changeHandler = (e) => {
+    setFormDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
     <>
       <form className="auth-form">
         <h2>Sign up</h2>
-        {formDetails.map((form) => {
+        {signupFormDetails.map((form) => {
           const { id, label, name, type } = form;
           return (
             <label key={id}>
               <div>{label}</div>
-              <input type={type} name={name} placeholder={`Enter ${name}`} />
+              <input
+                onChange={changeHandler}
+                value={formDetails[name]}
+                type={type}
+                name={name}
+                placeholder={`Enter ${name}`}
+              />
             </label>
           );
         })}
         <button>Singup</button>
-        <Link className="link">Already have an account</Link>
+        <Link to="/login" className="link">
+          Already have an account
+        </Link>
       </form>
     </>
   );
